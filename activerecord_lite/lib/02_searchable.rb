0bdb74028f475edc230c2c7fa3ff3456c45b1aa4
App Academy Student <aa@Apps-Mac-mini-4.local>
2019-02-22 17:20:38 -0500

    Finish Searchable module and mix in Searchable to SQLObject

--- activerecord_lite/lib/02_searchable.rb
+++ activerecord_lite/lib/02_searchable.rb
@@ -1,12 +1,24 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    colsets = params.keys.map { |col| col.to_s + " = ? AND "}.join[0..-5]
+    values = params.values
+    results = DBConnection.execute(<<-SQL, *values)
+    SELECT 
+      * 
+    FROM
+      #{self.table_name}
+    WHERE 
+      #{colsets}
+    SQL
+    return results if results.empty?
+    results.map { |hash_obj| self.new(hash_obj) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
